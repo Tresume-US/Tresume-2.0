992d437c92c35854a485fe61d0e34aff284e8c82
Mariya-Sherine <141424695+Mariya-Sherine@users.noreply.github.com>
2024-03-26 21:06:44 +0530

    update

--- Tresume-Express/timesheet.js
+++ Tresume-Express/timesheet.js
@@ -403,7 +403,7 @@ router.post("/Candidateviewdetails", async (req, res) => {
       }
       var request = new sql.Request();
 
-      var query = "SELECT TM.id,TM.admincomment,TM.billableamt, CONCAT(T.firstname, ' ', T.lastname) as Candidate, TM.projectid, TM.day1, TM.day2, TM.day3, TM.day4, TM.day5, TM.day6, TM.day7, TM.totalamt,TM.totalhrs,TM.status, TM.details,TM.clientapproved FROM Timesheet_Master TM INNER JOIN Trainee T ON TM.traineeid = T.traineeid WHERE TM.id='" + req.body.tid + "'";
+      var query = "SELECT TM.id,TM.lastUpdated_by,TM.admincomment,TM.billableamt, CONCAT(T.firstname, ' ', T.lastname) as Candidate, TM.projectid, TM.day1, TM.day2, TM.day3, TM.day4, TM.day5, TM.day6, TM.day7, TM.totalamt,TM.totalhrs,TM.status, TM.details,TM.clientapproved FROM Timesheet_Master TM INNER JOIN Trainee T ON TM.traineeid = T.traineeid WHERE TM.id='" + req.body.tid + "'";
 
 
       console.log(query);
