72057a09e8d39e8fa8213e6404942880f3fd4a37
Venkat-Tresume <149203012+Venkat-Tresume@users.noreply.github.com>
2024-03-29 20:04:03 +0530

    Revert "update"

--- src/app/view-details/view-details.component.ts
+++ src/app/view-details/view-details.component.ts
@@ -5,8 +5,6 @@ import { ViewDetailsService } from './view-details.service';
 import { MessageService } from 'primeng/api';
 import { ChangeDetectorRef } from '@angular/core';
 import { Directive, HostListener } from '@angular/core';
-import { DatePipe } from '@angular/common';
-
 
 @Component({
   selector: 'app-view-details',
@@ -69,16 +67,9 @@ export class ViewDetailsComponent {
     }
   }
 
-  constructor(private cdr: ChangeDetectorRef, private router: Router, private cookieService: CookieService, private service: ViewDetailsService, private messageService: MessageService, private route: ActivatedRoute, private renderer: Renderer2,private datePipe: DatePipe) { }
 
-  getFormattedDate(date: Date | null): string {
-    return date ? this.datePipe.transform(date, 'dd/MM') || '' : '';
-  }
-  getNextDay(date: Date, daysToAdd: number): Date {
-    const nextDay = new Date(date);
-    nextDay.setDate(nextDay.getDate() + daysToAdd);
-    return nextDay;
-}
+
+  constructor(private cdr: ChangeDetectorRef, private router: Router, private cookieService: CookieService, private service: ViewDetailsService, private messageService: MessageService, private route: ActivatedRoute, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     this.loading = true;
