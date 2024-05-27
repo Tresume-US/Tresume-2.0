1390831bafffa7dd93ff45d241c644ad47553a6d
Mariya Sherine <149203252+mariya-sherine-tresume@users.noreply.github.com>
2024-05-27 19:48:04 +0530

    Updates_Landing _Page

--- src/app/landing-page/home/home.component.ts
+++ src/app/landing-page/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit , Renderer2, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +7,44 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  activeSection: string = '';
+  private observer: IntersectionObserver;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
     window.scrollTo(0,0);
+    this.setupIntersectionObserver();
+  }
+  ngOnDestroy(): void {
+    this.observer.disconnect();
+  }
+
+  setupIntersectionObserver(): void {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
+
+    this.observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.activeSection = entry.target.id;
+          this.renderer.addClass(entry.target, 'active-section');
+        } else {
+          this.renderer.removeClass(entry.target, 'active-section');
+        }
+      });
+    }, options);
+
+    document.querySelectorAll('section').forEach(section => {
+      this.observer.observe(section);
+    });
+  }
+
+  scrollToSection(id: string): void {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
   }
   isScrolled = false;
   isNavbarCollapsed = false;
@@ -19,9 +53,17 @@ export class HomeComponent implements OnInit {
   }
 
 
-  // @HostListener('window:scroll', [])
-  // onWindowScroll() {
-  //   const offset = window.scrollY;
-  //   this.isScrolled = offset > 50;
-  // }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.scrollY;
+    this.isScrolled = offset > 50;
+  }
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  
+
 }
+
