2a083b87dc5177c239a514dabbdfdac1c465e251
Mariya Sherine <149203252+mariya-sherine-tresume@users.noreply.github.com>
2024-06-10 19:59:43 +0530

    commits_new_req

---
+++ src/app/recruiter-view-jobs/recruiter-view-jobs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpEvent, HttpParams, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecruiterViewJobsService {
+  public endpoint = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getJobPostingList(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'getJobPostingList', request);
+}
+
+
+}
+export interface ResponseDetails {
+  flag?: any;
+  result?: any;
+}
